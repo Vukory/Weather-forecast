67d4a87fba24c3d1af841e260e11a83f252a5cd7
Merjem Bajramovic <merjembajramovic8@gmail.com>
2019-12-18 13:12:49 +0100

    Start from scratch

--- script.js
+++ script.js
@@ -1,38 +1,55 @@
-$(document).ready(() => {
-    $(() => {
-        var today = new Date();
-        var day = today.getDate();
-        var month = today.getMonth();
-        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        var year = today.getFullYear();
+/** API key for OpenWeatherMap. */
+const apiKey = '9dcd8991f9f548f9d28bc5cf37a80c4e';
 
-        today = day + ' ' + months[month] + ' ' + year;
-        $("#currentDate").text("Today, " + today);
+/** Base URL to access API endpoints. */
+const baseUrl = 'https://api.openweathermap.org/data/2.5';
+
+/** The particular API endpoint we're interested in. */
+const endpoint = '/forecast';
+
+/** Whenever we want to make a request, just dash the query at the end of this. */
+const requestTemplate = `${baseUrl}${endpoint}?appid=${apiKey}&q=`;
+
+/** The index of the items in the list of temperatures we actually want. */
+const itemsOfInterest = [0, 8, 16, 24, 32, 39];
+
+const liTemplate =
+    `
+  <li>
+    <p class="humidity">Humidity: {{humidity}}</p>
+    <p class="pressure">Pressure: {{pressure}}</p>
+    <p class="temp">Temperature: {{temp}}</p>
+    <p class="temp-min">Minimum Temperature: {{temp_min}}</p>
+    <p class="temp-max">Maximum Temperature: {{temp_max}}</p>
+  </li>`;
+
+function onRequest() {
+    console.log("Put in a query.");
+    const query = $("#search-query").val();
+    const requestUrl = requestTemplate + query;
+
+    $.get(requestUrl, function(data, textStatus, jqXhr) {
+        console.log('Succesfully made request: ', data);
+        const city = data.city;
+        const country = city.county;
+
+        if (country)
+            $("#location").html(city.name + ", " + country);
+        else
+            $("#location").html(city.name);
+
+        const ul = $("#weather-items");
+        ul.html("");
+
+        for (let i = 0; i < itemsOfInterest.length; i++) {
+            const item = data.list[i];
+            const main = item.main;
+            let liItem = liTemplate;
+
+            for (const key in main)
+                liItem = liItem.replace(`{{${key}}}`, main[key]);
+
+            ul.append(liItem);
+        }
     });
-    let cities = [];
-    $(".display").hide();
-    $('#submitBtn').click(function(c) {
-        c.preventDefault();
-        var city = $('#inputCity').val(); //fetches the value of city
-        if (city !== "") {
-            $.ajax({
-                type: 'GET',
-                url: 'https://api.openweathermap.org/data/2.5/weather?' + city + '&appid=1b8a3ee8a0341c0b64756ef743026d5d',
-                dataType: 'jsonp',
-                success: function(data) {
-                    $(".display").show();
-                    cities.push(city);
-                    $('#weather').empty().text(data.weather[0].description);
-                    $("#city").empty().text(data.name);
-                    $('#temperature').empty().text(data.main.temp + "°C");
-                    $('#pressure').empty().append("Pressure: " + data.main.pressure + " hPa");
-                    $('#humidity').empty().append("Humidity: " + data.main.humidity + " %");
-                    $('#minTemp').empty().append("Min temperature: " + data.main.temp_min + "°C");
-                    $('#maxTemp').empty().append("Max temperature: " + data.main.temp_max + "°C");
-                    $('weatherIcon').attr('src', 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png');
-                    $("#display").fadeIn("slow", function() {});
-                }
-            })
-        } else $("#inputCity").attr("placeholder", "Molimo unesite ime grada")
-    });
-});
+}
